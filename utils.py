ff736652a93d66c4fd2521747d4631d401846da9
Toby Boyne <tobyboyne@gmail.com>
2020-03-18 13:15:16 +0000

    Changed initialisation of empty sets to properly create a set, not dict

--- utils.py
+++ utils.py
@@ -7,7 +7,7 @@ lowercase_letters = ascii_lowercase[:-1]
 def get_connections(word_list):
 	"""Returns a dictionary of each letter in the alphabet, where the value is
 	a set of all the letters that are linked to it based on a word list"""
-	connections = {letter: {} for letter in lowercase_letters}
+	connections = {letter: set() for letter in lowercase_letters}
 	for word in word_list:
 		for i, c in enumerate(word[:-1]):
 			connections[c].add(word[i + 1])
@@ -19,6 +19,6 @@ def get_linked_coords(W, H):
 	"""Returns a dictionary of each coordinate in the grid of size (W, H), and the list of all
 	points that are directly linked to that coordinate (vertical, horizontal, diagonal)
 	Converts from a geometrical board to an arbitrarily shape, considering only how adjecent tiles are connected"""
-	linked_coords = {}
+	linked_coords = set()
 	for (x, y) in product(range(W), range(H)):
 		pass
