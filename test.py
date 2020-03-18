d77fe87e930fe4a5f739fa6f93f1b34eee8e9876
Toby Boyne <tobyboyne@gmail.com>
2020-03-18 12:39:10 +0000

    Wrote test for get_connections()

---
+++ test.py
@@ -0,0 +1,19 @@
+import utils
+from utils import lowercase_letters
+
+def test_get_connections():
+	word_list = ['alpha', 'beta']
+	connections = utils.get_connections(word_list)
+	expected_connections = {letter: {} for letter in lowercase_letters}
+	result = {
+		'a': {'l', 'h', 't'},
+		'l': {'a', 'p'},
+		'p': {'l', 'h'},
+		'h': {'p', 'a'},
+		'b': {'e'},
+		'e': {'b', 't'},
+		't': {'e', 'a'}
+	}
+	expected_connections.update(result)
+
+	assert connections == expected_connections
