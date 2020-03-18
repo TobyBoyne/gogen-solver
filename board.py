4a3a0ad0d5fec409a91fc0444d16903a650612d8
Toby Boyne <tobyboyne@gmail.com>
2020-03-18 12:51:10 +0000

    Wrote basic structure for Board class

--- board.py
+++ board.py
@@ -1,5 +1,7 @@
 from itertools import product
 
+from utils import get_connections
+
 class Tile:
 	def __init__(self, letter=''):
 		self.letter = letter
@@ -7,3 +9,20 @@ class Tile:
 
 	def add_link(self, other):
 		self.children.append(other)
+
+
+class Board:
+	def __init__(self, start_values):
+		self.tiles = start_values
+		W, H = start_values.shape
+		self.coords = product(range(W), range(H))
+		self.links = {c:[] for c in self.coords}
+
+		self.link_tiles()
+
+	def link_tiles(self):
+		for tile in self.tiles:
+			pass
+
+	def solve(self, word_list):
+		connections = get_connections(word_list)
