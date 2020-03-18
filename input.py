5f95662e1e7fc28f268c25526b7d9a0c6d5f9307
Toby Boyne <tobyboyne@gmail.com>
2020-03-18 14:18:21 +0000

    Created UI to allow user input

--- input.py
+++ input.py
@@ -1,6 +1,16 @@
 import tkinter as tk
+from itertools import product
 
+def create_UI(W, H):
+	window = tk.Tk()
+	entries = []
+	for (x, y) in product(range(W), range(H)):
+		entry = tk.Entry()
+		entries.append(entry)
+		entry.config(font=32, width=2)
+		entry.grid(row=x, column=y)
+	return window
 
-window = tk.Tk()
-
-window.mainloop()
+if __name__ == "__main__":
+	master = create_UI(5, 5)
+	master.mainloop()
