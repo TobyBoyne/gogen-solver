52aeb3427e46fd6cbac7fd0a4851834a60a229a5
Toby Boyne <tobyboyne@gmail.com>
2020-03-19 13:12:46 +0000

    Added test for single_lowercase_letter

---
+++ test_ui.py
@@ -0,0 +1,13 @@
+import ui
+
+def test_single_lowercase_letter():
+	test_inputs = (
+		('a', True),
+		('', True),
+		('X', False),
+		('ab', False),
+		('2', False)
+	)
+
+	for s, expected_output in test_inputs:
+		assert ui.single_lowercase_letter(s) == expected_output
