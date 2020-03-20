051186bbc6a383a70e476f0d17f2d3efd5dc119d
Toby Boyne <tobyboyne@gmail.com>
2020-03-20 13:45:01 +0000

    Wrote logic to create Tile objects from value array

--- board.py
+++ board.py
@@ -12,15 +12,17 @@ class Tile:
 	def add_link(self, other):
 		self.children.append(other)
 
+	def __repr__(self):
+		return f"Tile({self.letter})"
 
 class Board:
 	def __init__(self, start_values):
-		self.tiles = start_values
-		W, H = start_values.shape
-		self.coords = product(range(W), range(H))
-		self.links = {c:[] for c in self.coords}
+		self.tiles = np.empty_like(start_values, dtype=object)
 
-		self.link_tiles()
+		for y, row in enumerate(start_values):
+			for x, value in enumerate(row):
+				self.tiles[y, x] = Tile(value)
+
 
 	def link_tiles(self):
 		for tile in self.tiles:
