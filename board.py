72de2310ffc7ceb0c2cc1fa7a86f314142312ab7
Toby Boyne <tobyboyne@gmail.com>
2020-03-20 16:50:06 +0000

    Added logic to solve method to make moves that are certain from starting conditions This is not sufficient to solve the gogen, but it provides a start and a foundation to the logic

--- board.py
+++ board.py
@@ -1,4 +1,4 @@
-from itertools import product
+from itertools import chain
 import numpy as np
 
 x = np.array([1, 0, 1])
@@ -14,7 +14,7 @@ class Board:
 	Contains solving logic"""
 	def __init__(self, start_values):
 		h, w = start_values.shape
-		self.start_letters = []
+		self.start_letters = set()
 
 		self.tiles = np.ones((h, w, len(lowercase_letters)))
 
@@ -22,19 +22,20 @@ class Board:
 			if letter:
 				i = letter_to_num(letter)
 				self.solve_tile(x, y, i)
-				self.start_letters.append((x, y, i))
+				self.start_letters.add((x, y, i))
 
 
 	def create_letter_mask(self, x, y, j):
 		"""Creates an AND mask centred around the point (x, y) so that the character stored in j can
 		only be placed adjacent to (x, y)
 		Used to apply a link between letters
-		1 values form a 3x3x25 grid"""
-		mask = np.zeros_like(self.tiles)
+		1 values form a 3x3x1 grid, and are 1 in every other slice"""
+		mask = np.ones_like(self.tiles)
 		# ensure that if x or y is 0, then index will remain positive
 		x_low = max(x-1, 0)
 		y_low = max(y-1, 0)
 
+		mask[:, :, j] = 0
 		mask[y_low:y+2, x_low:x+2, j] = 1
 		return mask
 
@@ -59,10 +60,12 @@ class Board:
 		if 0 in tile_count:
 			ValueError(f"The letter(s) {np.argwhere(tile_count==0)} cannot be placed on the grid")
 
-		solved_letters = np.argwhere(tile_count==1)
+		solved_letters = np.argwhere(tile_count==1).flat
+		out = []
 		for i in solved_letters:
 			y, x = np.argwhere(self.tiles[:, :, i])[0]
-			yield x, y, i
+			out.append((x, y, i))
+		return out
 
 	def check_tiles_solved(self):
 		"""Check if any of the tiles only have one possible letter that can be placed there
@@ -72,14 +75,16 @@ class Board:
 			raise ValueError(f"No letter can be placed at the coordinate(s) {np.argwhere(letter_count==0)}")
 
 		solved_tiles = np.argwhere(letter_count==1)
+		out = []
 		for y, x in solved_tiles:
-			i = np.argwhere(self.tiles[y, x, :])[0]
-			yield x, y, i
+			i = np.argwhere(self.tiles[y, x, :])[0][0]
+			out.append((x, y, i))
+		return out
 
 
 	def solve_tile(self, x, y, i):
 		"""Show that a single tile is solved by applying the mask"""
-		mask = self.create_letter_mask(x, y, i)
+		mask = self.create_tile_mask(x, y, i)
 		self.tiles = np.logical_and(self.tiles, mask)
 
 	def solve(self, word_list):
@@ -89,18 +94,27 @@ class Board:
 		 - exactly one possible space will be placed there, and removed from dictionary
 		 Through iteration and deduction, all letters will be placed"""
 		connections = get_connections(word_list)
-		used_letters = self.start_letters
-
+		solved_letters = self.start_letters
+		new_solved_letters = self.start_letters
+		# TODO: when there is no certain move, find the letter with the fewest possible positions and determine
+		# - where the next may go
 		# main solving loop - repeat until no more letters have been added
-		while used_letters:
+		while new_solved_letters:
 			# use masks to find where new letters can be placed
-			for x, y, i in used_letters:
+			for x, y, i in new_solved_letters:
 				for j in connections[i]:
-					mask = self.create_letter_mask(x, y, i)
+					mask = self.create_letter_mask(x, y, j)
 					self.tiles = np.logical_and(self.tiles, mask)
 
 			# check if any tiles can now be solved
-
+			new_solved_letters = set()
+			for x, y, i in self.check_letters_solved() + self.check_tiles_solved():
+				if (x, y, i) not in solved_letters:
+					self.solve_tile(x, y, i)
+					new_solved_letters.add((x, y, i))
+					solved_letters.add((x, y, i))
+			print(new_solved_letters, "\n\n\n")
+		print(self.tiles)
 
 
 
