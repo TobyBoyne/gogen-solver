11a1aa853f9ebd85f80dbbcbe1c18e2f1fc5dd80
Toby Boyne <tobyboyne@gmail.com>
2020-03-20 22:34:34 +0000

    Converted UI into a class

--- ui.py
+++ ui.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import numpy as np
 from itertools import product
 from utils import lowercase_letters
 
@@ -6,19 +7,22 @@ def single_lowercase_letter(s):
 	"""Validates input in each square so that only one lowercase letter can be added to each Entry"""
 	return len(s) <= 1 and s in lowercase_letters
 
-
-def create_UI(W, H):
-	window = tk.Tk()
-	entries = []
-	for (x, y) in product(range(W), range(H)):
-		entry = tk.Entry()
-		entries.append(entry)
-		entry.config(font=32, width=2,
-					 validate="key",
-					 vcmd=(entry.register(single_lowercase_letter), '%P'))
-		entry.grid(row=x, column=y)
-	return window
+class UserInterface(tk.Frame):
+	def __init__(self, parent, size, *args, **kwargs):
+		tk.Frame.__init__(self, parent, **kwargs)
+		# self.label = tk.Label(text="Hello, world")
+		# self.label.pack(padx=10, pady=10)
+		entries = []
+		for x, y in product(range(size[0]), range(size[1])):
+			entry = tk.Entry()
+			entries.append(entry)
+			entry.config(font=("Arial", 44), width=2, justify=tk.CENTER,
+						 validate="key",
+						 vcmd=(entry.register(single_lowercase_letter), '%P'))
+			entry.grid(row=x, column=y)
+			
 
 if __name__ == "__main__":
-	master = create_UI(5, 5)
+	master = tk.Tk()
+	UserInterface(master, (5, 5)).grid()
 	master.mainloop()
