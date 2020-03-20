09d28faae0138aa9db327610d5ea485bc6671240
Toby Boyne <tobyboyne@gmail.com>
2020-03-20 23:33:46 +0000

    Added test for valid_word_list

--- test_ui.py
+++ test_ui.py
@@ -1,4 +1,4 @@
-from ui import single_lowercase_letter
+from ui import single_lowercase_letter, valid_word_list
 
 def test_single_lowercase_letter():
 	test_inputs = (
@@ -11,3 +11,16 @@ def test_single_lowercase_letter():
 
 	for s, expected_output in test_inputs:
 		assert single_lowercase_letter(s) == expected_output
+
+def test_valid_word_list():
+	test_inputs = (
+		('a', True),
+		('', True),
+		(', b', True),
+		('cd', True),
+		('2', False),
+		('x?', False)
+	)
+
+	for s, expected_output in test_inputs:
+		assert valid_word_list(s) == expected_output
